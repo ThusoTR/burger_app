e49a3dd490349b98043bb6e99b273120ab9ba487
ThusoTR <67108860+ThusoTR@users.noreply.github.com>
2021-02-12 15:35:25 +0200

    Do the form

--- src/Componets/Checkout/Checkout.js
+++ src/Componets/Checkout/Checkout.js
@@ -1,42 +1,63 @@
 import React, {Component} from 'react'
 import Aux from '../../hoc/Aux2'
 import Burger from '../Burger/Burger'
-                                                                                                                                                                                                                                                                                                      
+import Styles from './Checkout.module.css'
+import { withRouter } from "react-router-dom";
+                                                                                                                                                                                                                                                                                                     
 
 class Checkout extends Component{
-    checkoutCancel = () =>{
+    state = {
+        checkOutContinue: false,
+        submiOrder: false,
+    }
 
+    CheckOutForm = (            
+    <div className = {Styles.CheckoutForm}>
+        <div>
+        <h2>Enter your contact data</h2>
+            <div>
+                <form>
+                    <input name = "userName" type="text" placeholder = "Enter your name"></input>
+                    <input name = "userEmail"type="email" placeholder = "Enter your email address"></input>
+                    <input name = "userAddress" type="text" placeholder = "Enter your street address"></input>
+                    <input name = "postCode" type="text" placeholder = "Enter your postal code"></input>  
+                </form>
+                <button onClick = {() => (this.continueCheckout())}>Submit Order</button>
+                <button onClick = {() => (this.checkoutCancel())} >Cancel Order</button>
+            </div>
+        </div>
+    </div>)
+    
+    checkoutCancel = () =>{
+        this.props.history.push('/')
     }
 
     continueCheckout = () =>{
-
+        this.setState({checkOutContinue: true})
     }
     render(){
         let currentIngredients = this.props.appManagement.getIngredients()
         return(
         <Aux>
-            {console.log("Ingredients in Burger Checkout = " + this.props.appManagement.ingredients)}
-            <h1>Enjoy your Burger!!</h1>
-            <Burger ingredients = {currentIngredients}/>
-            <div>
-                <button>Cancel</button>
-                <button >Continue</button>
-            </div>
+            <div className = {Styles.CheckoutForm}>
+                <div className = {Styles.BurgerContainer}>
+                    
+                    {console.log("Ingredients in Burger Checkout = " + this.props.appManagement.ingredients)}
+                    <h2>Enjoy your Burger!!</h2>
+                    <Burger ingredients = {currentIngredients}/>
+                    {!this.state.checkOutContinue ? (                    
+                    <div>
+                        <button onClick = {() => (this.checkoutCancel())}>Cancel</button>
+                        <button onClick = {() => (this.continueCheckout())}>Continue</button>
+                    </div>)
+                    : null}
 
-            <div>
-                <h1>Enter your contact data</h1>
-                <form>
-                  <input name = "userName" type="text" placeholder = "Enter your name"></input>
-                  <input name = "userEmail"type="email" placeholder = "Enter your email address"></input>
-                  <input name = "userAddress" type="text" placeholder = "Enter your street address"></input>
-                  <input name = "postCode" type="text" placeholder = "Enter your postal code"></input>  
-                </form>
-                <button >Continue</button>
+                </div>
             </div>
-            
+             {this.state.checkOutContinue ? this.CheckOutForm : null}
         </Aux>
         )
     }
 }
 
-export default Checkout
+export default withRouter(Checkout)
